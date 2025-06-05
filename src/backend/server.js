9f3340f12b9a7f410a3a88cd737aaba63e566295
ruizt <ruiztyler24@gmail.com>
2025-06-05 19:16:54 -0400

    Fix API endpoint URL by removing double slash in UserPage

--- src/backend/server.js
+++ src/backend/server.js
@@ -8,11 +8,11 @@ dotenv.config();
 
 // initializing express app
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
     'http://localhost:5173',
     'http://127.0.0.1:5173',
-    'https://steam-game-finder-orcin.vercel.app/'
+    'https://steam-game-finder-orcin.vercel.app'
 ];
 
 //to make changes switch routes back to 'http://localhost:5000' for development
